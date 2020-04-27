a2b23e4e9325c0662b0d0c89a0ad04164b978fcb
vvvvvoin <aszx7009@gmail.com>
2020-04-27 23:17:29 +0900

    Update 집합의_표현.java

--- SW_Test/src/baekJoonSW/Exam04_집합의_표현.java
+++ SW_Test/src/baekJoonSW/Exam04_집합의_표현.java
@@ -10,48 +10,64 @@ public class Exam04_집합의_표현 {
 	static StringTokenizer st;
 	static int N, tc;
 	static int operate, n1,n2;
-	static ArrayList<Set> list;
+	static int[] parent;
 	public static void main(String[] args) throws Exception{
 		br = new BufferedReader(new InputStreamReader(System.in));
 		st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		tc = Integer.parseInt(st.nextToken());
-		list = new ArrayList<>();
+		parent = new int[N+1];
+		
+		for(int i = 1; i<=N ; i++) {
+			parent[i] = i;
+		}
+		
+		
 		for(int i = 0; i <tc; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			operate = Integer.parseInt(st.nextToken());
 			n1 = Integer.parseInt(st.nextToken());
 			n2 = Integer.parseInt(st.nextToken());
 			if(operate == 0) {
-				for(int p =0; p < list.size(); p++) {
-					if(!list.get(p).set.contains(n1)) {
-						list.add(new Set(new ArrayList<Integer>()));
-						
-					}
-				}
-			}else if(operate == 1) {
-				if(list.contains(n1) || list.contains(n2)) {
-					System.out.println("YES");
-				}else System.out.println("NO");
+				union(n1, n2);
+			}else {
+				same(n1, n2);
 			}
+
 		}
 		
 	}
-	static class Set{
-		private ArrayList<Integer> set;
-
-		public ArrayList<Integer> getSet() {
-			return set;
-		}
-
-		public void setSet(ArrayList<Integer> set) {
-			this.set = set;
-		}
-
-		public Set(ArrayList<Integer> set) {
-			super();
-			this.set = set;
-		}
+	
+	static int find(int a) {
+		if(a == parent[a]) {
+			return a;
+		}else 
+			parent[a] = find(parent[a]);
+			return parent[a];
+	}
+	
+	static void union(int i, int j) {
+		i = find(i);
+		j = find(j);
 		
+		if(i > j) {
+			parent[i] = j;
+		}else parent[j] = i;
+	}
+
+	static void same(int i, int j) {
+		i = find(i);
+		j = find(j);
+		if(i == j) {
+			System.out.println("YES");
+		}else System.out.println("NO");
 	}
 }
+
+
+
+
+
+
+
+
